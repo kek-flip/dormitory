2a920e5ef9dd91dfb1d413fead57fc1d63d7daee
Maxim Davydov <norm.pochta.maxa@gmail.com>
2022-12-20 19:49:03 +0300

    Added validation to staff model

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -1,2 +1,5 @@
 class Staff < ApplicationRecord
+  validates_presence_of :user_id, message: 'Необходимо связать сотрудника с пользователем'
+  validates_uniqueness_of :user_id, message: 'Сотрудник связанный с этим пользователем уже существует'
+  validates_presence_of :rank, message: 'Необходимо указать должность'
 end
