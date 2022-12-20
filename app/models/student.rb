9739325089dca3676a2a0fe3ae63b4364827b45e
Maxim Davydov <norm.pochta.maxa@gmail.com>
2022-12-20 23:49:10 +0300

    Changed models validation from has_one to belongs_to

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,6 @@
 class Student < ApplicationRecord
-  has_one :users
-  validates_presence_of :users, message: 'Необходимо связать студента с пользователем'
+  belongs_to :user
+  validates_presence_of :user, message: 'Необходимо связать студента с пользователем'
   validates_uniqueness_of :user_id, message: 'Студент связанный с этим пользователем уже существует'
   validates_presence_of :room, message: 'Необходимо указать комнату проживания'
 end
