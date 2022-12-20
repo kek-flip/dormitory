cc9cd144f5fe3c32249218f0593fb229e0d82b5c
Maxim Davydov <norm.pochta.maxa@gmail.com>
2022-12-20 20:35:51 +0300

    Configured session controller

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,10 +1,21 @@
 class SessionController < ApplicationController
+  skip_before_action :authenticate, only: %i[login create]
+
   def login
   end
 
   def create
+    if user = User.authenticate(params[:login], params[:password])
+      session[:current_user_id] = user.id
+      redirect_to "/"  # Перенаправить на главную
+    else
+      redirect_to session_login_path, message: "Неверный логин или пароль"
+    end
   end
 
   def logout
+    session.delete(:current_user_id)
+    @_current_user = nil
+    redirect_to session_login_path
   end
 end
