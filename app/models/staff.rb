de9c12b7e48695b8ac21a7f1d5f70b452c9506d3
Maxim Davydov <norm.pochta.maxa@gmail.com>
2022-12-21 00:45:43 +0300

    Fixed validations to working state

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -1,6 +1,5 @@
 class Staff < ApplicationRecord
   belongs_to :user
-  validates_presence_of :user, message: 'Необходимо связать сотрудника с пользователем'
   validates_uniqueness_of :user_id, message: 'Сотрудник связанный с этим пользователем уже существует'
   validates_presence_of :rank, message: 'Необходимо указать должность'
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,5 @@
 class Student < ApplicationRecord
   belongs_to :user
-  validates_presence_of :user, message: 'Необходимо связать студента с пользователем'
   validates_uniqueness_of :user_id, message: 'Студент связанный с этим пользователем уже существует'
   validates_presence_of :room, message: 'Необходимо указать комнату проживания'
 end
