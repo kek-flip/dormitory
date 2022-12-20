5fe3fb1ae796fcb311b74f579633795033569db7
Maxim Davydov <norm.pochta.maxa@gmail.com>
2022-12-20 19:30:24 +0300

    Added validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates_uniqueness_of :login, message: 'Имя пользователя уже существует'
+  validates_presence_of :login, message: 'Имя пользователя не должно быть пустым'
+  validates_presence_of :password, message: 'Пароль пользователя не должно быть пустым'
+  validates_confirmation_of :password, message: 'Пароли различны'
+  validates_presence_of :password_confirmation, message: 'Повторите пароль'
 end
