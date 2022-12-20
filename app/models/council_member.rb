4ab863c166eb9e5f999bb8111d733bed73358220
Maxim Davydov <norm.pochta.maxa@gmail.com>
2022-12-20 19:55:52 +0300

    Added validation to council member model

--- app/models/council_member.rb
+++ app/models/council_member.rb
@@ -1,2 +1,5 @@
 class CouncilMember < ApplicationRecord
+  validates_presence_of :student_id, message: 'Необходимо связать члена студсовета со студентом'
+  validates_uniqueness_of :student_id, message: 'Данный студент уже состоит в студсовете'
+  validates_presence_of :rank, message: 'Необходимо указать должность'
 end
