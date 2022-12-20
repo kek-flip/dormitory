d0ba0e313bfeee97e1353182460f5f4cb496f79e
Maxim Davydov <norm.pochta.maxa@gmail.com>
2022-12-20 20:49:09 +0300

    Fixed text student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,5 @@
 class Student < ApplicationRecord
   validates_presence_of :user_id, message: 'Необходимо связать студента с пользователем'
   validates_uniqueness_of :user_id, message: 'Студент связанный с этим пользователем уже существует'
-  validates_presence_of :room, message: 'Необходимо укзать комнату проживания'
+  validates_presence_of :room, message: 'Необходимо указать комнату проживания'
 end
