d3c762b4fbc7411375f1689a62756293f1848672
Maxim Davydov <norm.pochta.maxa@gmail.com>
2022-12-21 00:39:10 +0300

    Removed validations

--- app/models/council_member.rb
+++ app/models/council_member.rb
@@ -1,6 +1,5 @@
 class CouncilMember < ApplicationRecord
   belongs_to :student
-  validates_presence_of :student, message: 'Необходимо связать члена студсовета со студентом'
   validates_uniqueness_of :student_id, message: 'Данный студент уже состоит в студсовете'
   validates_presence_of :rank, message: 'Необходимо указать должность'
 end
