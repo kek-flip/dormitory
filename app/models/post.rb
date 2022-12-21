14d1fd25bdf609741d225856b1fc0e7cbaa7c1b1
Maxim Davydov <norm.pochta.maxa@gmail.com>
2022-12-21 20:05:57 +0300

    Added post migration and validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  belongs_to: council_member
+  validates_presence_of :title, message: 'У новости должен быть заголовок'
+  validates_presence_of :text, message: 'Новость не может быть пустой'
 end
--- db/migrate/20221221170141_create_posts.rb
+++ db/migrate/20221221170141_create_posts.rb
@@ -1,7 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-
+      t.belongs_to :council_member
+      t.string :title
+      t.string :text
+      
       t.timestamps
     end
   end
