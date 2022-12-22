8e3eae1c7d734225e8428492d73fc1b4785edbc7
Maxim Davydov <norm.pochta.maxa@gmail.com>
2022-12-22 12:57:54 +0300

    Added null: false to post migration

--- db/migrate/20221221170141_create_posts.rb
+++ db/migrate/20221221170141_create_posts.rb
@@ -2,8 +2,8 @@ class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
       t.belongs_to :council_member
-      t.string :title
-      t.string :text
+      t.string :title, null: false
+      t.string :text, null: false
       
       t.timestamps
     end
