57ce9a7b9cfb72fde3363d70f003138d91a36a77
Maxim Davydov <norm.pochta.maxa@gmail.com>
2022-12-25 17:42:10 +0300

    Added user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    subject { User.new(login: 'test', password: 'test', name: 'Ivan', surname: 'Ivanov') }
+    it { should validate_uniqueness_of(:login).with_message('Имя пользователя уже существует') }
+    it { should validate_presence_of(:login).with_message('Укажите логин') }
+
+    it { should validate_presence_of(:password).with_message('Пароль пользователя не должно быть пустым') }
+    it { should validate_length_of(:password).with_message('Пароль должен быть 6 или более символов') }
+    it { should validate_confirmation_of(:password).with_message('Пароли различны') }
+    it { should validate_presence_of(:password_confirmation).with_message('Повторите пароль') }
+
+    it { should validate_presence_of(:name).with_message('Укажитие имя') }
+    it { should validate_presence_of(:surname).with_message('Укажитие фамилию') }
+  end
 end
