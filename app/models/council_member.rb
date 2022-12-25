6187f71d4dd69aacd904c01f07f4199fe7cf319a
Maxim Davydov <norm.pochta.maxa@gmail.com>
2022-12-25 18:06:05 +0300

    Added validation to council member model

--- app/models/council_member.rb
+++ app/models/council_member.rb
@@ -1,6 +1,7 @@
 class CouncilMember < ApplicationRecord
   belongs_to :student
   validates_uniqueness_of :student_id, message: 'Данный студент уже состоит в студсовете'
+  validates_presence_of :student_id, message: 'Необходимо связать члена студсовета со студентом'
   validates_each :student_id do |record, attr, value|
     record.errors.add(attr, message: "Не существует студента с id #{value}") if Student.find_by_id(value).nil?
   end
