6511169b0967c7165dfce67c0601ebaeab507dd7
Maxim Davydov <norm.pochta.maxa@gmail.com>
2022-12-25 18:12:47 +0300

    Changed profile route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'users/new'
   post 'users/create'
-  get 'users/:login', to: 'users#show'
+  get 'profile', to: 'users#show'
 
   get 'session/login'
   post 'session/create'
